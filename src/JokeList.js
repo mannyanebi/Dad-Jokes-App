2bed54054467501316397800430957a7c299c1ab
Anebi Emmanuel <anebiemmanuel@gmail.com>
2020-07-22 16:22:04 +0100

    Added Upvoting and Downvoting functionality to Joke component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import Joke from './Joke'
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import "./JokeList.css";
+
 
 
 class JokeList extends Component{
@@ -14,6 +17,14 @@ class JokeList extends Component{
         this.state = {
             jokes: []
         }
+
+        // this.handleVote = this.handleVote.bind(this)
+    }
+
+    handleVote(id, delta){
+        this.setState(state => ({
+            jokes: state.jokes.map(joke => joke.id === id ? {...joke, votes: joke.votes + delta} : joke)
+        }))
     }
 
     async componentDidMount(){
@@ -23,7 +34,7 @@ class JokeList extends Component{
             let res = await axios.get('https://icanhazdadjoke.com/', 
             {headers: {Accept: 'application/json'}})
 
-            newJokes.push(res.data.joke)
+            newJokes.push({id: uuidv4(), text: res.data.joke, votes: 0})
         }
         this.setState({
             jokes: newJokes
@@ -37,13 +48,19 @@ class JokeList extends Component{
                     <h1 className="JokeList-title">
                         <span>Dad</span> Jokes
                     </h1>
-                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="Laughing Emoji" />
                     <button className="JokeList-getmore">New Jokes</button>
                 </div>
                 
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(joke => (
-                        <div>{joke}</div>
+                        <Joke 
+                        key={joke.id} 
+                        text={joke.text} 
+                        votes={joke.votes} 
+                        upvote={() => this.handleVote(joke.id, 1)} 
+                        downvote={() => this.handleVote(joke.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
