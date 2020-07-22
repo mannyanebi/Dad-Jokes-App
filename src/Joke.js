b12a6815df4299f64b5878a544540618141c99c3
Anebi Emmanuel <anebiemmanuel@gmail.com>
2020-07-22 16:52:18 +0100

    Styled Joke component and added Laughing Emoji

--- src/Joke.js
+++ src/Joke.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
+import "./Joke.css"
 
 class Joke extends Component {
+
+    "#4CAF50"
+    "#8BC34A"
+    "#CDDC39"
+    "#FFE"
+
     render(){
         return(
             <div className="Joke">
                 <div className="Joke-buttons">
                     <i className="fas fa-arrow-up" onClick={this.props.upvote} />
-                        <span>{this.props.votes}</span>
+                        <span className="Joke-votes">{this.props.votes}</span>
                     <i className="fas fa-arrow-down" onClick={this.props.downvote}/>
                 </div>
                 <div className="Joke-text">{this.props.text}</div>
+                <div className="Joke-smiley">
+                    <i className="em em-rolling_on_the_floor_laughing" />
+                </div>
             </div>
         )
     }
